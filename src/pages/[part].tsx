e678362edb594c35adf60ef606b4ea09a2c7a887
y-shinonome <y.shinonomail@gmail.com>
2021-12-03 14:24:52 +0900

    #15-add: 投稿記事表示用ページ作成

---
+++ src/pages/[part].tsx
@@ -0,0 +1,72 @@
+import type { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import { client } from '../api/microCMS'
+import BlogCard from '../components/blogCard'
+import Pagination from '../components/pagination'
+
+type Props = {
+  posts: post[]
+  pageCount: {
+    totalCount: number
+    part: number
+  }
+}
+
+const PostsSplit: NextPage<Props> = ({ posts, pageCount }) => {
+  return (
+    <>
+      <ul className="px-3">
+        {posts.map((post, index) => (
+          <li key={index}>
+            <BlogCard post={post} />
+          </li>
+        ))}
+      </ul>
+      <Pagination
+        totalCount={pageCount.totalCount}
+        currentPage={pageCount.part}
+      />
+    </>
+  )
+}
+
+export const getStaticPaths = async () => {
+  //postsAPIからデータの総数を取得するためにidのみ取得する
+  const data = await client.get({
+    endpoint: 'posts',
+    queries: { fields: 'id' },
+  })
+  //postsデータの総数を元にルーティング用の配列を作成する関数
+  const range = (start: number, end: number) =>
+    [...Array(end - start + 1)].map((_, i) => start + i)
+  //5件毎に分割して投稿記事表示ページを作成する
+  const paths = range(1, Math.ceil(data.totalCount / 5)).map((number) => ({
+    params: { part: `${number}` },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const offset = Number(params?.part) * 5 - 5
+  const postData = await client.get({
+    endpoint: 'posts',
+    queries: { limit: 5, offset: offset },
+  })
+  const pageCount = {
+    totalCount: postData.totalCount,
+    part: Number(params?.part),
+  }
+
+  return {
+    props: {
+      posts: postData.contents,
+      pageCount: pageCount,
+    },
+  }
+}
+
+export default PostsSplit
