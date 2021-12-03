36d7b3bc4da5f04793aad2a0b9bc3e35dab354d0
y-shinonome <y.shinonomail@gmail.com>
2021-12-04 00:52:04 +0900

    #18-update: 投稿ページ作成 pagerコンポーネントのスタイル修正

--- src/components/pager.tsx
+++ src/components/pager.tsx
@@ -8,45 +8,38 @@ type props = {
     title: string
   }[]
   index: number
+  className?: string
 }
 
-const Pager: React.FC<props> = ({ pages, index }) => {
+const Pager: React.FC<props> = ({ pages, index, className }) => {
   return (
-    <div className="flex flex-wrap justify-between px-6 w-full sm:px-0">
-      <div className="mb-3 mr-5">
-        {pages[index + 1] !== undefined && (
-          <>
-            <p className="text-lg font-medium">Older</p>
-            <Link href={`/posts/${pages[index + 1].id}`}>
-              <a>
-                <Button>
-                  <div className="mr-1 w-4">
-                    <BsArrowLeft />
-                  </div>
-                  <span className="text-left">{pages[index + 1].title}</span>
-                </Button>
-              </a>
-            </Link>
-          </>
-        )}
-      </div>
-      <div>
-        {pages[index - 1] !== undefined && (
-          <>
-            <p className="text-lg font-medium">Newer</p>
-            <Link href={`/posts/${pages[index - 1].id}`}>
-              <a>
-                <Button>
-                  <span className="text-left">{pages[index - 1].title}</span>
-                  <div className="ml-1 w-4">
-                    <BsArrowRight />
-                  </div>
-                </Button>
-              </a>
-            </Link>
-          </>
-        )}
-      </div>
+    <div className={`flex flex-wrap justify-between w-full ${className}`}>
+      {pages[index + 1] !== undefined && (
+        <div className="mb-3 mr-5">
+          <p className="font-medium">Older</p>
+          <Link href={`/posts/${pages[index + 1].id}`}>
+            <a>
+              <Button className="py-[2px] px-2 text-sm">
+                <BsArrowLeft className="mr-1" />
+                {pages[index + 1].title}
+              </Button>
+            </a>
+          </Link>
+        </div>
+      )}
+      {pages[index - 1] !== undefined && (
+        <div>
+          <p className="font-medium">Newer</p>
+          <Link href={`/posts/${pages[index - 1].id}`}>
+            <a>
+              <Button className="py-[2px] px-2 text-sm">
+                {pages[index - 1].title}
+                <BsArrowRight className="ml-2" />
+              </Button>
+            </a>
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
