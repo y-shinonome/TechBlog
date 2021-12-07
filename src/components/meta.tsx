f811ef03453e7637659c6a9aea5fa548c35e5211
y-shinonome <y.shinonomail@gmail.com>
2021-12-08 06:52:05 +0900

    #21-update: メタデータの設定 汎用og画像とdescriptionを設定

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -9,8 +9,8 @@ type props = {
 
 const Meta: React.FC<props> = ({
   subTitle,
-  description = '',
-  imageUrl = 'https://images.microcms-assets.io/assets/8fd73e43936d4dc891b4a5238994ad6b/db3bd44fb11b4a9b8d7baeb0703f7fd8/sample.png',
+  description = '職業プログラマではないけど趣味でエンジニアリングをしています。モノづくり好きが高じてJamstack構成の爆速ブログを作ってみました。',
+  imageUrl = 'https://images.microcms-assets.io/assets/8fd73e43936d4dc891b4a5238994ad6b/f024a0c3614240b2811dbc43f7135ab7/cube.png',
   type = 'website',
 }) => {
   const title =
