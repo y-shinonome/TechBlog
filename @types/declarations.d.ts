c1274991de4fcd129be74513d52e10bc559ba704
y-shinonome <y.shinonomail@gmail.com>
2021-12-10 22:48:15 +0900

    update: API名の変更

--- @types/declarations.d.ts
+++ @types/declarations.d.ts
@@ -1,8 +1,8 @@
 //microcms-js-sdkのcreateClient関数のための型拡張
 namespace NodeJS {
   interface ProcessEnv {
-    readonly SERVICE_DOMAIN: string
-    readonly API_KEY: string
+    readonly MICROCMS_SERVICE_DOMAIN: string
+    readonly MICROCMS_API_KEY: string
   }
 } 
 
