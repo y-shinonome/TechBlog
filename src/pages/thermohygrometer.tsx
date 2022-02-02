b35c97f0948d70aa4b0a706fca45c6efe51d8fb3
y-shinonome <y.shinonomail@gmail.com>
2022-02-03 08:05:32 +0900

    #31-add: 温湿度測定データをグラフで表示する

--- src/pages/thermohygrometer.tsx
+++ src/pages/thermohygrometer.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
 import { query, collection, orderBy, limit, getDocs } from 'firebase/firestore'
 import { firestore } from '../utils/firebase'
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
 
 type measurement = {
-  datetime: Date,
+  datetime: string,
   temperature: number,
   humidity: number,
 }[]
@@ -18,27 +20,57 @@ const Thermohygrometer: NextPage = () => {
   },[])
 
   const fetchMesurement = async () => {
-    const q = query(collection(firestore, 'thermohygrometer'), orderBy('datetime','desc'), limit(10))
+    const q = query(collection(firestore, 'thermohygrometer'), orderBy('datetime','desc'), limit(24))
     const querySnapshot = await getDocs(q)
     const snapshot:measurement = querySnapshot.docs.map((doc) => {
-      const datetime = new Date(doc.data().datetime.seconds*1000)
+      const datetime = dayjs(doc.data().datetime.seconds * 1000).format('YYYY/MM/DD HH:mm')
       return ({
         datetime: datetime,
         temperature: doc.data().temperature,
         humidity: doc.data().humidity,
       })
     })
-    setMeasurementData(snapshot)
+    setMeasurementData(snapshot.reverse())
+  }
+
+  const roundNearest5 = (x: number) => {
+    return Math.round((x * 2 / 10 )) * 10 / 2
   }
 
   return (
-    <>
-      <ul>
-        {measurementData.map((measurement, index) => (
-          <li key={index}>{`${measurement.datetime} : ${measurement.temperature}℃ : ${measurement.humidity}%`}</li>
-        ))}
-      </ul>  
-    </>
+    <div className='mb-20'>
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart data={measurementData} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+            <Line name='気温' type='monotone' dataKey='temperature' stroke='#CC3333' yAxisId={1} isAnimationActive={false} />
+            <Line name='湿度' type='monotone' dataKey='humidity' stroke='#3333CC' yAxisId={2} isAnimationActive={false} />
+            <CartesianGrid stroke='#ccc' strokeDasharray='5 5'/>
+            <Legend verticalAlign="top"/>
+            <XAxis 
+              dataKey='datetime'
+              dy={5}
+              tickFormatter={(tickItem) => dayjs(tickItem).format('H:mm')}
+            />
+            <YAxis 
+              yAxisId={1} 
+              unit='℃' 
+              stroke = "#CC3333"
+              width={45}
+              tickCount={6}
+              domain={[(dataMin: number) => (roundNearest5(dataMin) - 5), (dataMax:number) => (roundNearest5(dataMax) + 15)]} 
+            />
+            <YAxis 
+              yAxisId={2} 
+              unit='%' 
+              stroke = "#3333CC" 
+              orientation='right' 
+              width={45}
+              tickCount={6}
+              domain={[(dataMin: number) => (roundNearest5(dataMin) - 15), (dataMax:number) => (roundNearest5(dataMax) + 5)]}
+            />
+            <Tooltip />
+          </LineChart>
+        </ResponsiveContainer>
+    </div>
   )
 }
 
