c08ffba5ad352a8c7bcf080c7e276845c4d37270
y-shinonome <y.shinonomail@gmail.com>
2022-02-15 17:39:34 +0900

    #33-add: 記事の目次コンポーネント追加 コンポーネント追加

--- src/components/tocbot.tsx
+++ src/components/tocbot.tsx
@@ -1,6 +1,8 @@
 import tocbot from 'tocbot'
 import { useEffect, useState } from 'react'
 import { usePath } from '../utils/client'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 
 type Props = {
   className?: string
@@ -18,6 +20,7 @@ const Tocbot: React.FC<Props> = ({ className }) => {
       tocSelector: '.toc',
       contentSelector: '.prose',
       headingSelector: 'h2, h3',
+      collapseDepth: 6,
     })
     return () => tocbot.destroy()
   }, [path])
@@ -29,7 +32,7 @@ const Tocbot: React.FC<Props> = ({ className }) => {
       {firstPath === 'posts' && (
         <div className={`${className}`}>
           <h3 className="mb-1 text-lg font-bold">Contents</h3>
-          <nav className="toc" />
+          <nav className="toc" css={toc} />
         </div>
       )}
     </>
@@ -37,3 +40,15 @@ const Tocbot: React.FC<Props> = ({ className }) => {
 }
 
 export default Tocbot
+
+const toc = css`
+  .is-active-link {
+    font-weight: 700;
+  }
+  .is-active-li {
+    background: rgba(128, 128, 128, 0.15);
+  }
+  .toc-list-item > ol > li {
+    padding-left: 10px;
+  }
+`
