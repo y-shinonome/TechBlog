88fadcd050ae89cc9dad1cefb72c1b753b41c349
y-shinonome <y.shinonomail@gmail.com>
2022-03-03 01:44:32 +0900

    update: カテゴリーリスト更新

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -5,14 +5,13 @@ type Props = {
 }
 
 const categoryList = [
-  { id: 'd2ps6ipc-4n', name: 'React' },
-  { id: 'k-ucibgv-e6j', name: 'TailwindCSS' },
-  { id: 'ung4k6hoc', name: 'その他' },
-  { id: 'q0jqdrn4c', name: 'Next.js' },
-  { id: 'gzy0dpzii6', name: 'TypeScript' },
   { id: 'w2cw_6sfs', name: 'ブログ構築' },
+  { id: 'gzy0dpzii6', name: 'TypeScript' },
   { id: 'mz2jnzo2e', name: 'RaspberryPi' },
   { id: 'ma6t88itybx', name: 'Docker' },
+  { id: 'k-ucibgv-e6j', name: 'TailwindCSS' },
+  { id: '9spc7kja6', name: '電子工作' },
+  { id: 'ung4k6hoc', name: 'その他' },
 ]
 
 const Categories: React.FC<Props> = ({ className }) => {
