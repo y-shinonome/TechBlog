2258e5fb49a64f9b5bffdafd2a19ce55d970ed99
y-shinonome <y.shinonomail@gmail.com>
2022-03-08 08:29:25 +0900

    #34-update: GoogleAnalytics対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import usePageView from '../utils/usePaseview'
+import GoogleAnalytics from '../components/googleAnalytics'
 import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView()
+
   return (
     <>
+      <GoogleAnalytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
